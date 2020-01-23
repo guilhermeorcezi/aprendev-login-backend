59f7848890a5aa1ccca6660facda723a2c2a4cdf
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-01-23 16:24:11 -0300

    minor issues solved

--- src/Controller/userController.js
+++ src/Controller/userController.js
@@ -6,11 +6,7 @@ module.exports = {
 
         const response = await axios.get('https://api.myjson.com/bins/kj4aq');
 
-        const { user, password} = response.data[0];
-
-      //  return res.json({response.data.user, pwd});
-        
-        if (response.data[0].user === login || response.data[1].user === login || response.data[2].user === login){
+         if (response.data[0].user === login || response.data[1].user === login || response.data[2].user === login){
             
             if(response.data[0].user === login && response.data[0].password === pwd
                 ||response.data[1].user === login && response.data[1].password === pwd
@@ -22,10 +18,8 @@ module.exports = {
                 return res.json({message: 'Wrong password'});  
             }
 
-
         }else{
             return res.json({message: 'user does not exist'});
         }
-        
     }
 };
