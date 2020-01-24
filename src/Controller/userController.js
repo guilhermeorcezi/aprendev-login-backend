1ee5bda27676f0ee4f2b77cf941c102182b4e2ee
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-01-23 22:48:25 -0300

    backend done

--- src/Controller/userController.js
+++ src/Controller/userController.js
@@ -2,24 +2,28 @@ const axios = require('axios');
 
 module.exports = {
     async store (req, res){
-        const { login, pwd } = req.body;
+        const { user, password } = req.body;
 
         const response = await axios.get('https://api.myjson.com/bins/kj4aq');
 
-         if (response.data[0].user === login || response.data[1].user === login || response.data[2].user === login){
-            
-            if(response.data[0].user === login && response.data[0].password === pwd
-                ||response.data[1].user === login && response.data[1].password === pwd
-                ||response.data[2].user === login && response.data[2].password === pwd
-                )
-            {
-                return res.json({logged_in: true});
-            }else{
-                return res.json({logged_in: true}); 
-            }
+        api = response.data;
 
+        let ver=false;
+        api.some(api => {
+            if (api.user === user){
+                ver=true;
+            }
+        });
+
+        if(ver){
+            api.forEach(api => {
+                if(api.user === user && api.password === password){
+                    return res.json({logged_in: true, user_exist: true});
+                }
+            });
+            return res.json({logged_in: false, user_exist: true});
         }else{
-            return res.json({logged_in: false});
+            return res.json({logged_in: false, user_exist: false});
         }
     }
 };
